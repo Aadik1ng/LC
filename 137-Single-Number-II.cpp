5b9f9271124d15bf3bd8f412abdabc33b33130a0
Aadik1ng <61167519+Aadik1ng@users.noreply.github.com>
2024-07-24 00:00:52 +0530

    [137] [Time Beats: 90.35%] [Memory Beats: 29.82%] - LeetPush

---
+++ 137-Single-Number-II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i += 3) {
+           
+            if (i + 2 >= nums.size() || nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1; 
+    }
+};
